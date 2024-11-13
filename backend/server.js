9fab5e1e9a4cb5f1bb20bb3f370277353e159008
Sudhakara H N <sudhakarahn29@gmail.com>
2024-11-13 14:37:49 +0530

    "Second commit : Added cors"

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('dotenv').config();
+const cors = require('cors');
 const dbConfig = require('./config/dbConfig')
 const app = express();
 const portFolioRoutes = require('./routes/portFolioRoutes')
