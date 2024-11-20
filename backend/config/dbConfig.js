e8fcb647c86d1a8000cdd947cce8a18a928045ab
Sudhakara H N <sudhakarahn29@gmail.com>
2024-11-20 23:56:38 +0530

    Updated

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, { 
-      useNewUrlParser: true, 
-      useUnifiedTopology: true, 
       serverSelectionTimeoutMS: 5000, // 5 seconds to select a server
       socketTimeoutMS: 45000, // 45 seconds for socket inactivity
     });
